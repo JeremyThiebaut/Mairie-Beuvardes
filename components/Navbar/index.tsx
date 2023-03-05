447026f5b629934ed406c1d75450d514c5c77cc8
Jeremy Thiebaut <jeremy.thiebaut.pro@gmail.com>
2023-03-05 22:49:49 +0100

    add sub menu to navbar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,16 +1,55 @@
 import Link from "next/link"
 import { useRouter } from "next/router"
+import { useState } from "react"
 import styles from "./styles.module.scss"
 
 const Navbar: React.FC = () => {
     const router = useRouter()
+    const [menuOpen, setMenuOpen] = useState(false)
+    const [secondMenuOpen, setSecondMenuOpen] = useState(false)
+    const [activeLink, setActiveLink] = useState("")
+
+    const handleLinkEnter = (link: string) => {
+        if (activeLink !== link) {
+            setActiveLink(link)
+            setMenuOpen(true)
+        }
+    }
+
+    const handleLinkLeave = (link: string) => {
+        if (activeLink === link) {
+            setActiveLink("")
+            setMenuOpen(false)
+            setSecondMenuOpen(false)
+        }
+    }
+
     return (
         <>
             <div className={styles.navbar}>
                 <nav>
-                    <Link href={"/"} className={router.asPath === "/" ? styles.active : ""}>Accueil</Link>
-                    <Link href={"/menu"} className={router.asPath === "/menu" ? styles.active : ""}>menu</Link>
-                    <Link href={"/test"} className={router.asPath === "/test" ? styles.active : ""}>test</Link>
+                    <div className={router.asPath === "/" ? styles.active : ""} onMouseEnter={() => handleLinkEnter("/")} onMouseLeave={() => handleLinkLeave("/")}>
+                        <p>Accueil</p>
+                        <div className={`${menuOpen && activeLink === "/" ? styles.show : styles.hide}`} onMouseEnter={() => setMenuOpen(true)} onMouseLeave={() => setMenuOpen(false)}>
+                            <div className={router.asPath === "/plusieurs" ? styles.active : ""} onMouseEnter={() => setSecondMenuOpen(true)} onMouseLeave={() => setSecondMenuOpen(false)}>
+                                <p>plusieurs</p>
+                                <div className={`${secondMenuOpen ? "" : styles.hide}`}>
+                                    <Link href={"/dernier"} className={router.asPath === "/dernier" ? styles.active : ""}>dernier</Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={router.asPath === "/test" ? styles.active : ""} onMouseEnter={() => handleLinkEnter("/test")} onMouseLeave={() => handleLinkLeave("/test")}>
+                        <p>Test</p>
+                        <div className={`${menuOpen && activeLink === "/test" ? styles.show : styles.hide}`} onMouseEnter={() => setMenuOpen(true)} onMouseLeave={() => setMenuOpen(false)}>
+                            <div className={router.asPath === "/plusieurs" ? styles.active : ""} onMouseEnter={() => setSecondMenuOpen(true)} onMouseLeave={() => setSecondMenuOpen(false)}>
+                                <p>autre</p>
+                                <div className={`${secondMenuOpen ? "" : styles.hide}`}>
+                                    <Link href={"/dernier"} className={router.asPath === "/dernier" ? styles.active : ""}>truc</Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <Link href={"/autre"} className={router.asPath === "/autre" ? styles.active : ""}>autre</Link>
                     <Link href={"/full"} className={router.asPath === "/full" ? styles.active : ""}>full</Link>
                 </nav>
@@ -20,59 +59,3 @@ const Navbar: React.FC = () => {
 }
 
 export default Navbar
-// import React, { useState } from "react";
-// import Link from "next/link";
-// import { useState } from "react";
-
-// type NavLinkProps = {
-//     href: string;
-//     text: string;
-// };
-
-// const NavLink: React.FC<NavLinkProps> = ({ href, text }) => {
-//     const [showSubNav, setShowSubNav] = useState(false);
-
-//     return (
-//         <li
-//             onMouseEnter={() => setShowSubNav(true)}
-//             onMouseLeave={() => setShowSubNav(false)}
-//         >
-//             <Link href={href}>
-//                 {text}
-//             </Link>
-//             {showSubNav && (
-//                 <ul>
-//                     <li>
-//                         <Link href={`${href}/sublink1`}>
-//                             Sublink 1
-//                         </Link>
-//                     </li>
-//                     <li>
-//                         <Link href={`${href}/sublink2`}>
-//                             Sublink 2
-//                         </Link>
-//                     </li>
-//                     <li>
-//                         <Link href={`${href}/sublink3`}>
-//                             Sublink 3
-//                         </Link>
-//                     </li>
-//                 </ul>
-//             )}
-//         </li>
-//     );
-// };
-
-// const Navbar: React.FC = () => {
-//     return (
-//         <nav>
-//             <ul>
-//                 <NavLink href="/" text="Link 1" />
-//                 <NavLink href="/link2" text="Link 2" />
-//                 <NavLink href="/link3" text="Link 3" />
-//             </ul>
-//         </nav>
-//     );
-// };
-
-// export default Navbar;
