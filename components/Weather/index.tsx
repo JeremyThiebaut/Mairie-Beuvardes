edc166ef23166ffca063035602b743c8b71e1602
JeremyThiebaut <jeremy.thiebaut.pro@gmail.com>
2023-03-06 15:42:34 +0100

    navbar add tools

--- components/Weather/index.tsx
+++ components/Weather/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios, { AxiosResponse } from "axios";
 import Image from "next/image";
-import styles from './styles.module.scss'
+import styles from "./styles.module.scss";
 
 interface WeatherData {
   name: string;
@@ -21,15 +21,16 @@ interface WeatherData {
   };
   wind: {
     speed: number;
-  }
+  };
 }
 
 const Weather = () => {
   const [weatherData, setWeatherData] = useState<WeatherData>(
     {} as WeatherData
   );
-  // const API_KEY = process.env.WEATHER_API ?? 'default_api_key';
-  const API_URL = "https://api.openweathermap.org/data/2.5/weather?q=beuvardes&units=metric&lang=fr&appid=fdd0fd85aff2f0d914c2fccf930caedb";
+  const API_KEY = process.env.OPENWEATHERMAP_API_KEY;
+  const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=beuvardes&units=metric&lang=fr&appid=${API_KEY}`;
+  console.log(API_URL);
   const getWeather = async () => {
     const result: AxiosResponse<WeatherData> = await axios(API_URL);
     setWeatherData(result.data);
@@ -45,31 +46,52 @@ const Weather = () => {
         <div className={styles.weather}>
           <div className={styles.weather__top_container}>
             <div className={styles.weather__top_top}>
-              <Image src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} alt="/" width={100} height={100} />
-              <p className={styles.weather__top_text}>{weatherData.weather[0].description}</p>
+              <Image
+                src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+                alt="/"
+                width={100}
+                height={100}
+              />
+              <p className={styles.weather__top_text}>
+                {weatherData.weather[0].description}
+              </p>
             </div>
             <div className={styles.weather__top_right_center}>
-              <p className={styles.weather__top_right}>{weatherData.main.temp}&#176;</p>
+              <p className={styles.weather__top_right}>
+                {weatherData.main.temp}&#176;
+              </p>
               <div className={styles.weather__top_right_container}>
-                <p className={styles.weather__top_right_down}>min {weatherData.main.temp_min.toFixed(0)}&#176;</p>
-                <p className={styles.weather__top_right_down}>max {weatherData.main.temp_max.toFixed(0)}&#176;</p>
+                <p className={styles.weather__top_right_down}>
+                  min {weatherData.main.temp_min.toFixed(0)}&#176;
+                </p>
+                <p className={styles.weather__top_right_down}>
+                  max {weatherData.main.temp_max.toFixed(0)}&#176;
+                </p>
               </div>
             </div>
           </div>
           <div className={styles.weather__bottom}>
-            <p className={styles.weather__bottom_title}>Météo à {weatherData.name}</p>
+            <p className={styles.weather__bottom_title}>
+              Météo à {weatherData.name}
+            </p>
             <div className={styles.weather__bottom_container}>
               <div>
                 <p className={styles.weather__bottom_description}>Ressenti</p>
-                <p className={styles.weather__bottom_text}>{weatherData.main.feels_like.toFixed(0)}&#176;</p>
+                <p className={styles.weather__bottom_text}>
+                  {weatherData.main.feels_like.toFixed(0)}&#176;
+                </p>
               </div>
               <div>
                 <p className={styles.weather__bottom_description}>Humidité</p>
-                <p className={styles.weather__bottom_text}>{weatherData.main.humidity}%</p>
+                <p className={styles.weather__bottom_text}>
+                  {weatherData.main.humidity}%
+                </p>
               </div>
               <div>
                 <p className={styles.weather__bottom_description}>Vent</p>
-                <p className={styles.weather__bottom_text}>{(weatherData.wind.speed * 3.6).toFixed(0)} km/h</p>
+                <p className={styles.weather__bottom_text}>
+                  {(weatherData.wind.speed * 3.6).toFixed(0)} km/h
+                </p>
               </div>
             </div>
           </div>
