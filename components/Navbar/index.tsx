1075cf17fffce4338c9cf0b1f21f347a72b8d59e
Jeremy Thiebaut <jeremy.thiebaut.pro@gmail.com>
2023-03-12 21:33:16 +0100

    add roll down menu to hover

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,204 +1,6 @@
-// import Link from "next/link";
-// import { useRouter } from "next/router";
-// import { useState } from "react";
-// import styles from "./styles.module.scss";
-
-// const Navbar: React.FC = () => {
-// 	const router = useRouter();
-// 	const [menuOpen, setMenuOpen] = useState(false);
-// 	const [secondMenuOpen, setSecondMenuOpen] = useState(false);
-// 	const [thirdMenuOpen, setThirdMenuOpen] = useState(false);
-// 	const [activeLink, setActiveLink] = useState("");
-
-// 	const handleLinkEnter = (link: string) => {
-// 		if (activeLink !== link) {
-// 			setActiveLink(link);
-// 			setMenuOpen(true);
-// 		}
-// 	};
-
-// 	const handleLinkLeave = (link: string) => {
-// 		if (activeLink === link) {
-// 			setActiveLink("");
-// 			setMenuOpen(false);
-// 			setSecondMenuOpen(false);
-// 		}
-// 	};
-
-// 	return (
-// 		<>
-// 			<div className={styles.navbar}>
-// 				<nav>
-// 					<div
-// 						className={router.asPath === "/macommune" ? styles.active : ""}
-// 						onMouseEnter={() => handleLinkEnter("/macommune")}
-// 						onMouseLeave={() => handleLinkLeave("/macommune")}
-// 					>
-// 						<p>Ma Commune</p>
-// 						<div
-// 							className={`${menuOpen && activeLink === "/macommune" ? styles.show : styles.hide
-// 								} ${styles.home}`}
-// 							onMouseEnter={() => setSecondMenuOpen(true)}
-// 							onMouseLeave={() => setSecondMenuOpen(false)}
-// 						>
-// 							<div
-// 								className={
-// 									router.asPath === "/conseil_municipal" ? styles.active : styles.test
-// 								}
-// 								onMouseEnter={() => setThirdMenuOpen(true)}
-// 								onMouseLeave={() => setThirdMenuOpen(false)}
-// 							>
-// 								<div>
-// 									<p>Conseil municipal</p>
-// 								</div>
-// 								{/* <div className={`${secondMenuOpen ? "" : styles.hide}`}>
-// 								<Link
-// 									href={"/dernier"}
-// 									className={
-// 										router.asPath === "/dernier" ? styles.active : styles.test
-// 									}
-// 								>
-// 									dernier
-// 								</Link>
-// 							</div> */}
-// 							</div>
-// 							<div
-// 								className={
-// 									router.asPath === "/comptes_rendus" ? styles.active : styles.test
-// 								}
-// 								onMouseEnter={() => setThirdMenuOpen(true)}
-// 								onMouseLeave={() => setThirdMenuOpen(false)}
-// 							>
-// 								<div>
-// 									<p>Comptes rendus</p>
-// 								</div>
-// 								{/* <div className={`${secondMenuOpen ? "" : styles.hide}`}>
-// 									<Link
-// 										href={"/dernier"}
-// 										className={
-// 											router.asPath === "/dernier" ? styles.active : styles.test
-// 										}
-// 									>
-// 										dernier
-// 									</Link>
-// 								</div> */}
-// 							</div>
-// 						</div>
-// 					</div>
-
-// 					<div
-// 						className={router.asPath === "/test" ? styles.active : ""}
-// 						onMouseEnter={() => handleLinkEnter("/test")}
-// 						onMouseLeave={() => handleLinkLeave("/test")}
-// 					>
-// 						<p>Mes demandes</p>
-// 						<div
-// 							className={`${menuOpen && activeLink === "/test" ? styles.show : styles.hide
-// 								} ${styles.test}`}
-// 							onMouseEnter={() => setMenuOpen(true)}
-// 							onMouseLeave={() => setMenuOpen(false)}
-// 						>
-// 							<div
-// 								className={router.asPath === "/plusieurs" ? styles.active : ""}
-// 								onMouseEnter={() => setSecondMenuOpen(true)}
-// 								onMouseLeave={() => setSecondMenuOpen(false)}
-// 							>
-// 								<p>autre</p>
-// 								<div className={`${secondMenuOpen ? "" : styles.hide}`}>
-// 									<Link
-// 										href={"/dernier"}
-// 										className={
-// 											router.asPath === "/dernier" ? styles.active : ""
-// 										}
-// 									>
-// 										truc
-// 									</Link>
-// 								</div>
-// 							</div>
-// 						</div>
-// 					</div>
-
-// 					<div
-// 						className={router.asPath === "/" ? styles.active : ""}
-// 						onMouseEnter={() => handleLinkEnter("/")}
-// 						onMouseLeave={() => handleLinkLeave("/")}
-// 					>
-// 						<p>Enfance jeunesse</p>
-// 						<div
-// 							className={`${menuOpen && activeLink === "/" ? styles.show : styles.hide
-// 								} ${styles.home}`}
-// 							onMouseEnter={() => setSecondMenuOpen(true)}
-// 							onMouseLeave={() => setSecondMenuOpen(false)}
-// 						>
-// 							<div
-// 								className={
-// 									router.asPath === "/plusieurs" ? styles.active : styles.test
-// 								}
-// 								onMouseEnter={() => setThirdMenuOpen(true)}
-// 								onMouseLeave={() => setThirdMenuOpen(false)}
-// 							>
-// 								<div>
-// 									<p>plusieurs</p>
-// 								</div>
-// 								<div className={`${secondMenuOpen ? "" : styles.hide}`}>
-// 									<Link
-// 										href={"/dernier"}
-// 										className={
-// 											router.asPath === "/dernier" ? styles.active : styles.test
-// 										}
-// 									>
-// 										dernier
-// 									</Link>
-// 								</div>
-// 							</div>
-// 						</div>
-// 					</div>
-
-// 					<div
-// 						className={router.asPath === "/" ? styles.active : ""}
-// 						onMouseEnter={() => handleLinkEnter("/")}
-// 						onMouseLeave={() => handleLinkLeave("/")}
-// 					>
-// 						<p>Vie Du Village</p>
-// 						<div
-// 							className={`${menuOpen && activeLink === "/" ? styles.show : styles.hide
-// 								} ${styles.home}`}
-// 							onMouseEnter={() => setSecondMenuOpen(true)}
-// 							onMouseLeave={() => setSecondMenuOpen(false)}
-// 						>
-// 							<div
-// 								className={
-// 									router.asPath === "/plusieurs" ? styles.active : styles.test
-// 								}
-// 								onMouseEnter={() => setThirdMenuOpen(true)}
-// 								onMouseLeave={() => setThirdMenuOpen(false)}
-// 							>
-// 								<div>
-// 									<p>plusieurs</p>
-// 								</div>
-// 								<div className={`${secondMenuOpen ? "" : styles.hide}`}>
-// 									<Link
-// 										href={"/dernier"}
-// 										className={
-// 											router.asPath === "/dernier" ? styles.active : styles.test
-// 										}
-// 									>
-// 										dernier
-// 									</Link>
-// 								</div>
-// 							</div>
-// 						</div>
-// 					</div>
-// 				</nav>
-// 			</div>
-// 		</>
-// 	);
-// };
-
-// export default Navbar;
-
 import React, { useState } from 'react';
 import Link from 'next/link';
+import styles from './styles.module.scss';
 
 type SubMenu = {
 	text: string;
@@ -219,6 +21,13 @@ type SubMenuLink = {
 const Navbar = () => {
 	const [subMenuState, setSubMenuState] = useState<{ [key: string]: boolean }>({}); // ex: string=false
 
+	const handleMouseLeave = (key: string) => {
+		setSubMenuState((prevState) => ({
+			...prevState,
+			[key]: false,
+		}));
+	};
+
 	const handleMouseEnter = (key: string) => {
 		setSubMenuState((prevState) => ({
 			...prevState,
@@ -226,11 +35,16 @@ const Navbar = () => {
 		}));
 	};
 
-	const handleMouseLeave = (key: string) => {
-		setSubMenuState((prevState) => ({
-			...prevState,
-			[key]: false,
-		}));
+	const toggleNav = (key: string) => {
+		setSubMenuState((prevState) => {
+			const newState = { ...prevState };
+			if (newState.hasOwnProperty(key)) {
+				newState[key] = !newState[key];
+			} else {
+				newState[key] = true;
+			}
+			return newState;
+		});
 	};
 
 	const subMenuLinks: SubMenuLink[] = [
@@ -242,6 +56,24 @@ const Navbar = () => {
 				{ text: 'Commissions municipales', url: "/commissionsMunicipales" },
 				{ text: 'Services municipaux', url: "/servicesMunicipaux" },
 				{
+					text: 'Bulletins municipaux',
+					subSubMenus: [
+						{ text: 'Petit beuvardois', url: "/petitBeuvardois" },
+						{ text: 'Petit journal', url: "/petitJournal" },
+					],
+				}, {
+					text: 'Bulletins municipaux',
+					subSubMenus: [
+						{ text: 'Petit beuvardois', url: "/petitBeuvardois" },
+						{ text: 'Petit journal', url: "/petitJournal" },
+					],
+				}, {
+					text: 'Bulletins municipaux',
+					subSubMenus: [
+						{ text: 'Petit beuvardois', url: "/petitBeuvardois" },
+						{ text: 'Petit journal', url: "/petitJournal" },
+					],
+				}, {
 					text: 'Bulletins municipaux',
 					subSubMenus: [
 						{ text: 'Petit beuvardois', url: "/petitBeuvardois" },
@@ -350,52 +182,57 @@ const Navbar = () => {
 	];
 
 	return (
-		<nav>
+		<nav className={styles.navbar}>
 			<ul>
 
 				{/* =========== */}
 				{subMenuLinks.map((subMenuLink, index) => (
 					<li
 						key={index}
-						onMouseEnter={() => handleMouseEnter(`link${index + 1}`)}
 						onMouseLeave={() => handleMouseLeave(`link${index + 1}`)}
 					>
-						<p>{subMenuLink.menu}</p>
+						<p
+							onMouseOver={() => {
+								// 	toggleNav(`link${index + 1}`)
+								handleMouseEnter(`link${index + 1}`)
+							}}
+
+						>
+							{subMenuLink.menu}
+
+						</p>
 						{subMenuState[`link${index + 1}`] && (
-							<div className="submenu">
+							<div className={styles.submenu}>
 
 								{/* =========== */}
 								{subMenuLink.subMenus.map((subMenu, subMenuIndex) => (
-									<div key={subMenuIndex}>
+									<div key={subMenuIndex} className={styles.submenu__container}>
 										{subMenu.url ? (
 											<Link href={subMenu.url}>{subMenu.text}</Link>
 										) : (
 
 											<p
-												onMouseEnter={() =>
-													handleMouseEnter(`text${index * 2 + subMenuIndex + 1}`)
-												}
-												onMouseLeave={() =>
-													handleMouseLeave(`text${index * 2 + subMenuIndex + 1}`)
-												}
+												onMouseLeave={() => handleMouseLeave(`text${index * 2 + subMenuIndex + 1}`)}
+												onMouseEnter={() => {
+													toggleNav(`text${index * 2 + subMenuIndex + 1}`)
+												}}
 											>
 												{subMenu.text}
 											</p>
 										)}
 										{subMenuState[`text${index * 2 + subMenuIndex + 1}`] && (
-											< div className="subsubmenu">
+											< div className={styles.subsubmenu}>
 
 												{/* =========== */}
 												{subMenu.subSubMenus ? subMenu.subSubMenus.map((subSubMenu, subSubMenuIndex) => (
 													<div key={subSubMenuIndex} >
 														<Link
 															href={subSubMenu.url}
-															onMouseEnter={() =>
-																handleMouseEnter(`text${index * 2 + subMenuIndex + 1}`)
-															}
-															onMouseLeave={() =>
-																handleMouseLeave(`text${index * 2 + subMenuIndex + 1}`)
-															}>
+															onMouseLeave={() => handleMouseLeave(`text${index * 2 + subMenuIndex + 1}`)}
+															onMouseEnter={() => {
+																toggleNav(`text${index * 2 + subMenuIndex + 1}`)
+															}}
+														>
 															{typeof subSubMenu === 'object' ? subSubMenu.text : subSubMenu}
 														</Link>
 													</div>
